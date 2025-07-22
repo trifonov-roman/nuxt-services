55746f102629adc9223d8501cbe9779890d6d5fd
Trifonov <rtrifonov@secureducation.ru>
2025-07-22 18:05:59 +0400

    min fix

--- app/components/Base/Checkbox/index.vue
+++ app/components/Base/Checkbox/index.vue
@@ -12,7 +12,7 @@ const isChecked = computed<boolean>({
     if (groupModel && props.value !== undefined) {
       return groupModel.value.includes(props.value)
     }
-    return model.value ?? false
+    return model.value ?? false // может быть undefined поэтому приводим к false,а model может не быть когда мы работаем через group
   },
   set(val) {
     if (groupModel && props.value !== undefined) {
@@ -26,7 +26,8 @@ const isChecked = computed<boolean>({
 
 <template>
   <label class="base-checkbox">
-    <input v-model="isChecked" type="checkbox" :disabled="disabled" :indeterminate="indeterminate"
+    <input
+v-model="isChecked" type="checkbox" :disabled="disabled" :indeterminate="indeterminate"
       class="base-checkbox__input">
     <span v-if="text" class="base-checkbox__label">{{ text }}</span>
   </label>
--- app/services/user/api.ts
+++ app/services/user/api.ts
@@ -1,16 +1,23 @@
 import type { GetUser } from "./type";
 
 export const getUser = () => {
-  return useApi<GetUser>("/user",{
-        key:'getUser'
+  return useApi<GetUser>("/user", {
+    key: "getUser",
   });
 };
 
 export const getUserFollowng = (username: string) => {
-  return useApi(`/user/blocks/{username}`,{
+  return useApi(`/user/blocks/{username}`, {
     dinamic: {
-      username
-    }
+      username,
+    },
   });
 };
 
+//Пример useMutation
+export const updateUser = async (body: any) => {
+  return useNuxtApp().$api<any>("/users/2", {
+    method: "PUT",
+    body,
+  });
+};
